f5bdfe68164f0181c0f8615d87137b64e6d1ef44
irisachi <115192614+irisachi@users.noreply.github.com>
2023-09-20 13:37:37 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include "dfa.h"
+#include "IntHashSet.h"
 
 
+
+typedef struct string string;
 
 int main() {
-    DFA dfa1 = new_DFA(6);
-    DFA_print(dfa1);
+    // DFA_print(dfa1);
+    IntHashSet test;
+    test = "CSC";
+    if (DFA_for_contains_CSC(test) == true) {
+        std:cout << "The string " << test << " is accepted";
+    }
+    else {
+        std:cout << "the string" << test << "is not accepted";
+    }
+    IntHashSet test2;
+    test2 = "ending"
+    if (DFA_for_contains_end(test2) == true){
+        cout<<"The string" <<test<< "is accepted";
+
+        else
+
+    }
+    //DFA_for_contains_CSC();
+    //DFA_for_contains_end();
+    //DFA_for_starts_with_vowel();
     return 0;
 }
+
+
 
 
